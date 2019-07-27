6c95b7fbc529e7d3b3f087c78fb50db9fdd4ca99
Huanhua Su <karawakara@gmail.com>
2019-07-27 01:38:14 -0700

    Initial the user login view, add bootstrap, add styles, fix static files path redirect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require( 'express' );
 var router = express.Router();
 
+var indexView = {
+  signInTitle: 'A Simple Nodebook',
+
+
+
+  copyright: 'Huanhua Su little project'
+};
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get( '/', function ( req, res, next ) {
+  // render index.html using view obj
+  res.render( 'index', indexView );
+} );
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var mustache = require( 'mustache' );
 var fs = require( 'fs' ); // this engine requires the fs module
 var indexRouter = require( './routes/index' );
 var usersRouter = require( './routes/users' );
-var app = express();
+var server = express();
 
 
 // define the template engine
-app.engine( 'html', function ( filePath, view, callback ) {
+server.engine( 'html', function ( filePath, view, callback ) {
     fs.readFile( filePath, function ( err, data ) {
         if ( err )
             return callback( err );
@@ -19,20 +19,27 @@ app.engine( 'html', function ( filePath, view, callback ) {
         return callback( null, renderedString );
     } )
 } );
-app.set( 'views', './views' );
-app.set( 'view engine', 'html' );
+server.set( 'views', './views' );
+server.set( 'view engine', 'html' );
 
 
 //var cookieParser = require('cookie-parser');
 //var logger = require('morgan');
 
-//app.use(logger('dev'));
-app.use( express.json() );
-app.use( express.urlencoded( { extended: false } ) );
-//app.use(cookieParser());
-app.use( express.static( path.join( __dirname, 'public' ) ) );
+//server.use(logger('dev'));
+server.use( express.json() );
+server.use( express.urlencoded( { extended: false } ) );
+//server.use(cookieParser());
 
-app.use( '/', indexRouter );
-app.use( '/users', usersRouter );
+/**
+ * serving static files
+ *                                  path to file
+ * http://localhost:3000/files/  +  images/kitten.jpg
+ */
+server.use( '/files', express.static( path.join( __dirname, 'public' ) ) );
 
-module.exports = app;
+
+server.use( '/', indexRouter );
+server.use( '/users', usersRouter );
+
+module.exports = server;
