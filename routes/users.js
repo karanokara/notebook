9edfdfaa41dbb2d6accc1e6cb557eafba93b1ea0
Huanhua Su <karawakara@gmail.com>
2019-07-29 18:07:21 -0700

    Start client side JS

--- routes/users.js
+++ routes/users.js
@@ -1,55 +1,27 @@
-var express = require( 'express' );
+var express = require('express');
 var router = express.Router();
-var tool = require( '../tool' );
+var tool = require('../tool');
 
 var view = {
   siteTitle: 'Notebook - User Notes',
   bodyClasses: 'user',
   username: 'HappyTester',
-  list: [
-    {
-      noteTitle: 'Suggestions',
-      noteExcert: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus ....'
-
-    },
-    {
-      noteTitle: 'Suggestions',
-      noteExcert: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus ....'
-
-    },
-    {
-      noteTitle: 'Suggestions',
-      noteExcert: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus ....'
-
-    },
-    {
-      noteTitle: 'Suggestions',
-      noteExcert: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus ....'
-
-    },
-  ],
-  stylesheets: [
-    'files/stylesheets/bootstrap.min.css',
-    'files/stylesheets/style.css',
-
-  ],
-  settings: {
-
-  },
   userSettingBtnImage: '/files/images/user-setting.svg',
   noteSettingBtnImage: '/files/images/note-setting.svg',
-  noteExpand: tool.readFile( 'public/images/arrow-down-s.svg' ),
-  appBottomMenu: tool.readFile( 'views/app-bottom-menu.html' ),
+  noteExpand: tool.readFile('public/images/arrow-down-s.svg'),
+  appBottomMenu: tool.readFile('views/app-bottom-menu.html'),
   logoImage: '/files/images/note-no-shadow.svg',
 };
 
 /* GET users listing. */
-router.get( '/', function ( req, res, next ) {
+router.get('/', function (req, res, next) {
+  var userFile = 'user';
 
-  view.body = tool.render( 'user', view );
+  view.body = tool.render('user', view);
+  view.userData = tool.fetchNoteData(userFile);
 
   // render index.html using view obj
-  res.render( 'index', view );
-} );
+  res.render('index', view);
+});
 
 module.exports = router;
--- tool.js
+++ tool.js
@@ -1,18 +1,21 @@
-var fs = require( 'fs' );
-var mustache = require( 'mustache' );
+var fs = require('fs');
+var mustache = require('mustache');
 
 var tools = {
     // root of path this this file folder ../notebook
-    readFile: function ( path ) {
-        var buffer = fs.readFileSync( path );
+    readFile: function (path) {
+        var buffer = fs.readFileSync(path);
 
         return buffer.toString();
     },
-    render: function ( viewTemplate, view ) {
+    render: function (viewTemplate, view) {
         var path = 'views/' + viewTemplate + '.html'
-        return mustache.render( this.readFile( path ), view );
+        return mustache.render(this.readFile(path), view);
+    },
+    fetchNoteData: function (fileName) {
+        var dataStr = this.readFile('data/' + fileName + '.json');
+        return JSON.parse(dataStr);
     }
-
 };
 
 
